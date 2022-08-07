60f6779b9848da0218c02ab4ab7765d6b3325bfc
sergANTstar <106014778+sergANTstar@users.noreply.github.com>
2022-08-08 00:01:06 +0300

    u

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ function onSearch(e) {
     refs.loadMore.classList.add('is-hidden')
     newsApiServer.query = e.currentTarget.elements.searchQuery.value;
 
-    if (newsApiServer.query === ' ' || newsApiServer.query === '') {
+    if (newsApiServer.query === null) {
         return Notify.info("Please, enter search text!")
     }
 
